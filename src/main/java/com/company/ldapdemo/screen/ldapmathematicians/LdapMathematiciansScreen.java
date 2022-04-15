c05f7081ff3230f18ccc66cb2b5a43765bd4440a
Nadezhda Shatalova <n.shatalova@haulmont.com>
2022-04-15 17:07:11 +0400

    add screens for Mathematicians and Scientists roles

---
+++ src/main/java/com/company/ldapdemo/screen/ldapmathematicians/LdapMathematiciansScreen.java
@@ -0,0 +1,10 @@
+package com.company.ldapdemo.screen.ldapmathematicians;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("l_LdapMathematiciansScreen")
+@UiDescriptor("ldap-mathematicians-screen.xml")
+public class LdapMathematiciansScreen extends Screen {
+}
---
+++ src/main/java/com/company/ldapdemo/screen/ldapscientists/LdapScientistsScreen.java
@@ -0,0 +1,10 @@
+package com.company.ldapdemo.screen.ldapscientists;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("l_LdapScientistsScreen")
+@UiDescriptor("ldap-scientists-screen.xml")
+public class LdapScientistsScreen extends Screen {
+}
